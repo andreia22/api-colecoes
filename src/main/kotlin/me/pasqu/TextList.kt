03ac01a0ab25965e08fdd6b86aaf3467b8e6dc00
Andreia Pasqual <pasqual1982.ap@gmail.com>
2021-03-17 16:48:20 -0300

    add list

---
+++ src/main/kotlin/me/pasqu/TextList.kt
@@ -0,0 +1,40 @@
+package me.pasqu
+
+fun main() {
+
+    val andreia = Funcionario( "andreia", 2000.0, "CLT")
+    val janaina = Funcionario( "janaina", 1000.0, "PJ" )
+    val amanda = Funcionario( "amanda", 3000.0, "CLT")
+
+    val funcionarios = listOf(andreia, janaina, amanda)
+
+    funcionarios.forEach { println(it) }
+    println("_______________________________________")
+    println(funcionarios.find{it.nome == "janaina"})
+    println("_______________________________________")
+
+    funcionarios
+        .sortedBy {it.salario}
+        .forEach { println(it) }
+
+    println("_______________________________________")
+
+    funcionarios
+        .groupBy {it.tipoContratacao}
+        .forEach { println(it) }
+
+}
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+    )
+{
+    override fun toString(): String =
+        """
+            nome: $nome
+            salario: $salario
+        """.trimIndent()
+
+}
